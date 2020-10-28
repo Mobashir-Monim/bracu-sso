1dae0dea4254ccf0c443f83248d651c683c7918c
Mobashir Monim <mobashirmonim@gmail.com>
2020-10-28 10:57:06 +0600

    debug

--- app/Http/Controllers/SSOController.php
+++ app/Http/Controllers/SSOController.php
@@ -39,7 +39,7 @@ class SSOController extends Controller
 
         $val = $helper->authenticatorParamDecompressor($request->stuff);
 
-        return redirect($val->redirect_uri . '?state=' . $val->state . '&code=' . $helper->createAuthCode($val, Passport::authCode()) . '&scope=' . $val->scope);
+        return redirect($val->redirect_uri . '?state=' . $val->state . '&code=' . $helper->createAuthCode($val, Passport::authCode())->id . '&scope=' . $val->scope);
     }
 
     public function exchangeCodeToken()
