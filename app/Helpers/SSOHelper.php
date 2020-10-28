75a36a04f844ba8cbc8ba98849a9ff291085ed28
Mobashir Monim <mobashirmonim@gmail.com>
2020-10-28 20:05:26 +0600

    debug

--- app/Helpers/SSOHelper.php
+++ app/Helpers/SSOHelper.php
@@ -81,7 +81,6 @@ class SSOHelper extends Helper
         ]));
 
         $payload = $this->base64url_encode(json_encode($payload));
-        dd($payload);
         $signature = hash_hmac('sha256', $header . '.' . $payload, $secret, true);
 
         return $header . '.' . $payload . '.' . $signature;
